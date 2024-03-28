5b4f6a837fc258f522e32dfa65579ec6dd4be115
Virendra Mali <viren@godrej.com>
2024-03-28 12:32:43 +0530

    Auth Files added

---
+++ auth.js
@@ -0,0 +1,10 @@
+function getUser() {
+    return {
+        name: 'Jhon',
+        email: 'XXXXXXXXXXXXXX'
+    }
+}
+
+function loginUser(user) {
+    console.log('Usuario logado com sucesso!')
+}
